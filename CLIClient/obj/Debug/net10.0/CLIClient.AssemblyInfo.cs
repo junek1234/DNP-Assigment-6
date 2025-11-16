4eb6080ea8141acadb9fa69411cf92c34df8219f
Piotr Junosz <piotrju2005@wp.pl>
2025-11-16 19:33:46 +0100

    removing double page issue

--- CLIClient/obj/Debug/net10.0/CLIClient.AssemblyInfo.cs
+++ CLIClient/obj/Debug/net10.0/CLIClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLIClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec584e26f710b200a26e63dc8b0d030739df72b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e06ed08333b4f23334b44209a0603b6ffc521e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLIClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLIClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
